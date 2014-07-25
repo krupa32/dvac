ea81c46609df6e55acf6c0f44dd633ed905d66b2
krupa <krupa32@gmail.com>
2014-07-25 15:27:54 +0530

    backup scripts changed to reflect dvac onsite settings

--- backup/backup.sh
+++ backup/backup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Configuration variables
-SITEDIR=/home/krupa/work/dvac
-SYNCDIR=/home/krupa/dvacbackup
-BACKUPDIR=/tmp
-MYSQLPASS=fossil27
+SITEDIR=/var/www/dvac
+SYNCDIR=/root/highcourtbackup
+BACKUPDIR=/mnt/usb/highcourtbackup
+MYSQLPASS=123456
 RETAIN=7
 
-BACKUPNAME=`date +%Y-%m-%d-%M`
+BACKUPNAME=`date +%Y-%m-%d`
 
 if [ ! -d $SYNCDIR ]
 then
@@ -22,16 +22,17 @@ fi
 # Take backup
 mysqldump -u root -p$MYSQLPASS --databases dvac --add-drop-database > $SYNCDIR/db.sql
 rsync -ru $SITEDIR/case/uploads $SYNCDIR
-zip -r $BACKUPDIR/$BACKUPNAME.zip $SYNCDIR/*
+tar -czf $BACKUPDIR/$BACKUPNAME.tgz $SYNCDIR/*
+#zip -r $BACKUPDIR/$BACKUPNAME.zip $SYNCDIR/*
 
 # remove older backups (if any)
-NUMBACKUPS=`ls -t1 $BACKUPDIR/*.zip | wc -l`
+NUMBACKUPS=`ls -t1 $BACKUPDIR/* | wc -l`
 #echo "numbackup=$NUMBACKUPS retain=$RETAIN" >> $BACKUPDIR/log.txt
 if [ $NUMBACKUPS -gt $RETAIN ]
 then
 	OLDBACKUPS=`expr $NUMBACKUPS - $RETAIN`
 	#echo "oldbackups=$OLDBACKUPS" >> $BACKUPDIR/log.txt
-	for i in `ls -t1 $BACKUPDIR/*.zip | head -n $OLDBACKUPS`
+	for i in `ls -tr1 $BACKUPDIR/* | head -n $OLDBACKUPS`
 	do
 		rm $i
 	done
